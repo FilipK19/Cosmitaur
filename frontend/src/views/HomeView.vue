8a0e9e0fb889583345710d6300d10d8db706c5f1
FilipK19 <filipkukic15@gmail.com>
2025-03-05 20:12:49 +0100

    the start of homepage

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,29 @@
 <template>
   <div class="home">
-    hi
+    <img src="@/assets/logo.jpg" class="logom">
+    <h1>Cosmitaur</h1>
   </div>
 </template>
 
 <script>
 
 </script>
+
+<style>
+.logom {
+  width: 250px;
+  height: 250 px;
+  position: relative;
+  right: 500px;
+}
+
+h1 {
+  font-size: 150px;
+  font-style: italic;
+  font-weight: 100;
+  position: relative;
+  top: -300px;
+  left: 200px;
+}
+
+</style>
