dbfe21634ea16b540b6b88055b44c80d1a875987
FilipK19 <filipkukic15@gmail.com>
2025-04-15 20:35:42 +0200

    making basic get route

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
 
+urldict = {
+  "videoId1": "q_TG6vNJPJE",
+  "videoId2": "FAB1xEe1A1c",
+  "videoId3": "4BpSUmOVr_4",
+  "videoId4": "dSlxRQ_u8RE",
+  "videoId5": "pe9OQaaiqNI",
+  "videoId6": "enQrOaZLC70",
+  "videoId7": "PQfkvC6RASA",
+  "videoId8": "GtUaeNQCqmM",
+  "videoId9": "QNv2aCh7MHc"
+}
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/url/{item_id}")
+async def read_item(item_id: str):
+    if item_id in urldict:
+        return {"item_id": item_id, "url": urldict[item_id]}
+    else:
+        raise HTTPException(status_code=404, detail="Item not found")
